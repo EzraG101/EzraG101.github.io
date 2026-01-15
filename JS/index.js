c1ec65c28f3b1e3d646e427be1ff68a87ad35712
Ezra Guerrero <ezga923@gmail.com>
2026-01-14 22:59:14 -0500

    profile pic

--- JS/index.js
+++ JS/index.js
@@ -1,5 +1,5 @@
 const picture =
-  "https://media.licdn.com/dms/image/v2/D4E03AQHflJHIs_2RSg/profile-displayphoto-crop_800_800/B4EZpvvypBIUAI-/0/1762811391045?e=1767830400&v=beta&t=FKAJ9AmtfPP5VUlpRTPJmXhDHq_WG0c4Y8ooyjKfpJU";
+  "https://media.licdn.com/dms/image/v2/D4E03AQHflJHIs_2RSg/profile-displayphoto-crop_800_800/B4EZpvvypBIUAI-/0/1762811391045?e=1770249600&v=beta&t=pBb-qilXuAcOxko1Hobs3Xid-NyyyH5UdYBQrwmlm4U";
 
 const description =
   "I am Ezra Guerrero Alvarez, a fourth-year undergraduate Mathematics student at MIT. I am interested in geometric topology, specifically Gauge theory, geometric flows, and their applications to modern problems in 3- and 4- manifold topology. <br/> <br/> This is my personal website and portfolio. Here, you can find a list of my <a href='research'>publications</a>, a <a href='resources'>library</a> of mathematical resources I've created, and my <a href='assets/cv.pdf'>CV</a>. <br/> <br/> You may contact me at ezrag{at}mit{dot}edu.";
